8fdb3807401807e4619114e62c87a658abdac214
romuloban <romulo88@hotmail.com>
2021-02-24 12:40:55 +0100

    add query param

--- pages/notes/[id].jsx
+++ pages/notes/[id].jsx
@@ -1,5 +1,11 @@
 import React from 'react'
+import { useRouter } from 'next/router'
+import { route } from 'next/dist/next-server/server/router';
 
-const Page = () => <div>Note page</div>
+const Page = () => {
+    const router = useRouter();
+    const { id } = router.query;
+    return <h1>Note {id}</h1>
+}
 
 export default Page;
