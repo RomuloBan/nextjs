c4c8ae830e2f5883d65ca59f05ae0211add49607
romuloban <romulo88@hotmail.com>
2021-02-24 15:36:49 +0100

    add theme-ui to NotesIndex

--- pages/notes/index.jsx
+++ pages/notes/index.jsx
@@ -1,24 +1,30 @@
-import React from 'react'
+/** @jsx jsx */ /** @jsxRuntime classic */ 
+import { jsx } from 'theme-ui'
 import Link from 'next/link'
+import Page from '..'
 
-const Page = () => {
-    const notes = new Array(15).fill(1).map((e, i) => ({id: i, title: `Note: ${i}`}))
+const NotesIndex =  () => {
+  const notes = new Array(15).fill(1).map((e, i) => ({id: i, title: `This is my note ${i}`}))
 
-    return (
-        <div>
-        <h1>Notes</h1>
+  return (
+    <div sx={{variant: 'containers.page'}}>
+      <h1>My Notes</h1>
 
+      <div sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', flexWrap: 'wrap'}}>
         {notes.map(note => (
-            <div>
+          <div sx={{width: '33%', p: 2}}>
             <Link key={note.id} href="/notes/[id]" as={`/notes/${note.id}`}>
-                <a>
-                <strong>{note.title}</strong>
-                </a>
+              <a sx={{textDecoration: 'none', cursor: 'pointer'}}>
+                <div sx={{variant: 'containers.card',}}>
+                  <strong>{note.title}</strong>
+                </div>
+              </a>
             </Link>
-            </div>
+          </div>
         ))}
-        </div>
-    )
+      </div>
+    </div>
+  )
 }
 
-export default Page;
+export default NotesIndex;
