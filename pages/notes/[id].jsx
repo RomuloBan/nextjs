329e50fde67ee291bb0547a84cd538c7d7ad12d9
romuloban <romulo88@hotmail.com>
2021-02-25 11:56:11 +0100

    add server side props

--- pages/notes/[id].jsx
+++ pages/notes/[id].jsx
@@ -1,17 +1,33 @@
 /** @jsx jsx */ /** @jsxRuntime classic */ 
 import { jsx } from 'theme-ui'
-import { useRouter } from 'next/router'
-import Link from 'next/link'
 
-const NotePage = () => {
-  const router = useRouter()
-  const { id }= router.query
-
+const NotePage = ({note}) => {
   return (
     <div sx={{variant: 'containers.page'}}>
-      <h1>Note: {id} </h1>
+      <h1>Note: {note.title} </h1>
     </div>
   )
 }
 
 export default NotePage;
+
+export async function getServerSideProps({req, res, params}) {
+    const response = await fetch(`http://localhost:3000/api/note/${params.id}`)
+    console.log('>>>>>>>>>', response);
+    if (!response.ok) {
+        res.setHeader('Location', '/notes')
+        res.statusCode = 302
+        res.end();
+        return {
+            props: {}
+        }
+    }
+
+    const {data} = await response.json();
+    if (data) {
+        return {
+            props: {note: data}
+        }
+    }
+
+}
